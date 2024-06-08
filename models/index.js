2599ba2d507031e4ee4ec62484c31da7a6c0ce32
jbm97 <macadamjake@gmail.com>
2024-06-08 14:57:13 -0400

    feat: edit models/index.js for deployment connection

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
-console.log("--PRINT--", process.env.MONGO_URI);
+
 // import models
 const User = require("./user");
 const Workout = require("./workouts");
@@ -8,12 +7,18 @@ const Food = require("./foodlog");
 const Goal = require("./goals");
 const Contact = require("./contact");
 
-mongoose.connect(process.env.MONGO_URI);
-
-const db = mongoose.connection;
-
-db.once("open", () => console.log(`Connected to MongoDB at Gains & Grains. PORT:${db.port}`));
-db.on("error", (error) => console.log("Database error \n", error));
+const uri = process.env.MONGO_URI;
+const clientOptions = { serverApi: { version: "1", strict: true, deprecationErrors: true } };
+async function run() {
+    try {
+        // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+        await mongoose.connect(uri, clientOptions);
+        await mongoose.connection.db.admin().command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
+}
+run().catch(console.dir);
 
 module.exports = {
     User,
